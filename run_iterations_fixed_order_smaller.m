dd59a5953987e62d5de3c9f4319e2e2141e023c4
iguanaus <johnpeurifoy@yahoo.com>
2017-04-24 23:10:16 -0400

    SMaller iteratins

---
+++ run_iterations_fixed_order_smaller.m
@@ -0,0 +1,34 @@
+values = [];
+myspects = [];
+c1 = 0;
+c2 = 0;
+data = load('data.mat');
+lambda = linspace(300, 800, 501)';
+omega = 2*pi./lambda;
+eps_silver = interp1(data.omega_silver,data.epsilon_silver,omega);
+eps_gold   = interp1(data.omega_gold,data.epsilon_gold,omega);
+
+botlim=10.0
+toplim=40.0
+
+for r1 = botlim:2:toplim;
+    for r2 = botlim:2:toplim;
+        for r3 = botlim:2:toplim;
+            for r4 = botlim:2:toplim;
+                spect = run_spectrum_advanced_four(r1,r2,r3,r4);
+                myspects = [myspects spect(1:5:501,1)];
+                values = [values ; [r1,r2,r3,r4]]; 
+            end
+        end
+        if rem(r2,1) ==0;
+            disp('Onr2');
+            disp(r1);
+        end
+    end
+    if rem(r1,1) ==0;
+       disp('Onr1');
+       disp(r1);
+    end
+end
+csvwrite('test_large_fixed_five.csv',myspects);
+csvwrite('test_large_fixed_five_val.csv',values);
---
+++ run_spectrum_advanced_four.m
@@ -0,0 +1,15 @@
+%THis runs the specrum with a silver core fixed.
+function spectrum = run_spectrum_advanced_five(r1,r2,r3,r4)
+lambda = linspace(300, 800, 501)';
+omega = 2*pi./lambda;
+data = load('data.mat');
+eps_silver = interp1(data.omega_silver,data.epsilon_silver,omega);
+eps_gold   = interp1(data.omega_gold,data.epsilon_gold,omega);
+eps_silica = 2.04*ones(length(omega), 1);
+eps_water  = 1.77*ones(length(omega), 1);
+% load data on epsilon
+
+% test case one: 40-nm-radius silver sphere in water
+eps = [eps_silver eps_gold eps_silver eps_gold eps_water];
+a = [r1 r2 r3 r4];
+spectrum = total_cs(a,omega,eps)/(pi*sum(a)^2);
