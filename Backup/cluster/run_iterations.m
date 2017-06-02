228272e7e35afb795cf66194e5402f942fcc2647
iguanaus <johnpeurifoy@yahoo.com>
2017-06-02 18:39:43 -0400

    removed csvs

--- Backup/cluster/run_iterations.m
+++
@@ -1,9 +0,0 @@
-values = [];
-myspects = [];
-for v = 20.0:0.05:70.0;
-    spect = run_spectrum(v);
-    myspects = [myspects spect([1,50,100,150,200,250,300,350,400,450,501],1)];
-    values = [values v];
-end
-csvwrite('test.csv',myspects);
-csvwrite('test_val.csv',values);
